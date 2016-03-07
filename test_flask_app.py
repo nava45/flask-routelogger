42cafb0dd7be9121a293018e8b1412c8e2db8db2
Navaneethan R <navaneethan.r@edgenetworks.in>
2016-03-07 18:14:59 +0530

    flask test app comments

--- test_flask_app.py
+++ test_flask_app.py
@@ -4,14 +4,14 @@ from flask_routelogger import RouteLogger
 app = Flask(__name__)
 app.config.update(EXCLUDE_ROUTES = ('/',))
 
+# Add Flask-RouteLogger config in your flask application
 rapp = RouteLogger(app, log_everything=True)
 
 
 
 @app.route('/')
 def test():
-    #raise "TTT"
-    return "hai"
+    return "Welcome to flask-routerlogger testing!!"
 
 
 if __name__ == '__main__':
